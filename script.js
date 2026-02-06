74516b0f826f9d21387a82850d73b90006406aab
Piyush kushwah <piyushkushwah002@gmail.com>
2026-02-06 15:41:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+// Ques 1:- Print numbers from 1 to 100 using a loop.
+
+// let i = 1;
+// while (i <= 100) {
+//     console.log(i);
+//     i++;
+// }
+
+
+// Ques 2:- Print all even numbers between 1 and 50.
+
+// let i = 1;
+// while(i <= 50){
+//     if(i%2==0){
+//         console.log(i);  
+//     }
+//     i++;
+// }
+
+
+// Ques 3:- Print all odd numbers between 1 and 50.
+
+// let i = 1;
+// while(i <= 50){
+//     if(i%2!=0){
+//         console.log(i);  
+//     }
+//     i++;
+// }
+
+
+// Ques 4:- Print numbers from 50 to 1 in reverse order.
+
+// let i = 50;
+// while(i>=1){
+//     console.log(i);
+//     i--;
+// }
+
+
+// Ques 5:- Find the sum of numbers from 1 to 100.
+
+// let i=1;
+// sum=0;
+
+// while(i<=100){
+//     sum=sum+i;
+//     i++;
+// }
+// console.log(sum);
+
+
+// Ques 6:- Print multiplication table of a given number.
+
+// let n = 5;
+
+// for (let i = 1; i <= 10; i++)
+//     console.log(n, "*", i, "=", n * i);
+
+
+// Ques 7:- Count numbers divisible by 3 between 1 and 100.
+
+// let n=3;
+// let count=0;
+
+// for(let i=1;i<=100;i++ ){
+//     if(i%3==0){
+//         count++;
+//     }
+// }
+// console.log(count);
+
+// Ques 8:- Print squares of numbers from 1 to 20.
+
+// for (let i = 1; i <= 20; i++) {
+//     console.log(i * i);
+// }
+
+
+// Ques 9:- Print cubes of numbers from 1 to 15.
+
+// for (let i = 1; i <= 15; i++) {
+//     console.log(i * i * i);
+// }
+
+
+// Ques 10:- Print each character of a string.
+
+// let str = "Hello";
+
+// for (let ch of str) {
+//     console.log(ch);
+// }
